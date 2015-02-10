fd23866ce576849fadf464c231411056c5e9b788
kyle billings <the.kyle.billings@gmail.com>
2015-02-10 14:46:08 -0800

    yay

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$(".faq-q").click( function () {
+  var container = $(this).parents(".faq-c");
+  var answer = container.find(".faq-a");
+  var trigger = container.find(".faq-t");
+  
+  answer.slideToggle(200);
+  
+  if (trigger.hasClass("faq-o")) {
+    trigger.removeClass("faq-o");
+  }
+  else {
+    trigger.addClass("faq-o");
+  }
+});
